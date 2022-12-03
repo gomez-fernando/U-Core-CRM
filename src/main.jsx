c6efb030e750a58b6c5df97427edc5b1caba1e2c
gomez-fernando <ferdgomez@hotmail.es>
2022-12-03 21:25:37 +0100

    Create navbar

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './components/Layout'
+import NewCustomer from './pages/NewCustomer'
+import Index from './pages/Index'
 
 const router = createBrowserRouter([
-  { path: '/', element: <h1>Home</h1> }
+  { 
+    path: '/', 
+    element: <Layout /> ,
+    children: [
+      {
+        index: true,
+        element: <Index />
+      },
+      {
+        path: '/clientes/nuevo', 
+        element: <NewCustomer /> 
+      }
+    ]
+  }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
