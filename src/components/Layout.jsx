2dc04fbef72c9204a6b0c96c221c4461a2e86b14
gomez-fernando <ferdgomez@hotmail.es>
2022-12-04 14:16:30 +0100

    Add new customer form and validation

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -10,8 +10,8 @@ const Layout = () => {
         <h2 className='text-4xl font-black text-center text-white'>CRM - Clientes</h2>
 
         <nav className="mt-10">
-          <Link className={`${location.pathname === '/' ? 'text-blue-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300 `} to='/'>Clientes</Link>
-          <Link className={`${location.pathname === '/clientes/nuevo' ? 'text-blue-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300 `} to='/clientes/nuevo'>Nuevo Cliente</Link>
+          <Link className={`${location.pathname === '/' ? 'text-white underline' : 'text-blue-300'} text-2xl block mt-2 `} to='/'>Clientes</Link>
+          <Link className={`${location.pathname === '/clientes/nuevo' ? 'text-white underline' : 'text-blue-300 hover:text-blue-600 '} text-2xl block mt-2 `} to='/clientes/nuevo'>Nuevo Cliente</Link>
         </nav>
       </aside>
       <main className='md:w-3/4 p-10 md:h-screen overflow-scroll'>
--- src/pages/NewCustomer.jsx
+++ src/pages/NewCustomer.jsx
@@ -1,8 +1,65 @@
-import React from 'react'
+import { useNavigate, Form, useActionData } from 'react-router-dom'
+import CustomerForm from '../components/CustomerForm'
+import Error from '../components/Error'
+
+export async function action({request}) {
+  // const formData = await request.formData()
+
+  // console.log([...formData])
+
+  const data = Object.fromEntries(await request.formData())
+  const email = data.email
+
+  // Validation
+  const errors = []
+  if(Object.values(data).includes('')){
+    errors.push('Todos los campos son obligatorios')
+  }
+
+  let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
+
+  if(!regex.test(email)){
+    errors.push('El email no es válido')
+  }
+
+  if(Object.keys(errors).length){
+    return errors
+  }
+  return null
+}
 
 function NewCustomer() {
+  const errors = useActionData()
+  const navigate = useNavigate()
+  console.log(errors);
+
   return (
-    <div>NewCustomer</div>
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Nuevo Cliente</h1>
+      <p className="mt-3">Llena todos los campos para registrar un nuevo cliente</p>
+
+      <div className="flex justify-end">
+        <button
+          className="bg-blue-800 text-white px-3 py-1 font-bold uppercase rounded-sm"
+          onClick={() => navigate(-1)}
+        >Volver</button>
+      </div>
+
+      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20" >
+        {errors?.length && errors.map((error, i) => <Error key={i}>{error}</Error>)}
+
+        <Form method='post' noValidate>
+          <CustomerForm />
+          <div className="flex justify-center">
+            <input
+              type="submit"
+              className="mt-5 w-1/2 mx-auto bg-blue-800 p-3 uppercase font-bold text-white text-lg rounded-md"
+              value="Registrar Cliente"
+            />
+          </div>
+        </Form>
+      </div>
+    </>
   )
 }
 
